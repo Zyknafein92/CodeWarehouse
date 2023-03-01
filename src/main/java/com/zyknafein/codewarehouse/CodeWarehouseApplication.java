d597f657f9a68777938da3e86427da3e25438a3f
Team1648 <junior.wf1648@gmail.com>
2023-03-01 13:54:12 +0100

    initial commit

---
+++ src/main/java/com/zyknafein/codewarehouse/CodeWarehouseApplication.java
@@ -0,0 +1,14 @@
+package com.zyknafein.codewarehouse;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyknafein/codewarehouse/CodeWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyknafein.codewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
